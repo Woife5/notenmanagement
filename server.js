2124e65bef8b8089a3aee435f3ab14a997a3b5f5
Wolfgang Schwendtbauer <wolfgang.schwendtbauer@gmail.com>
2018-03-01 11:34:58 +0100

    Connecting to database; Server essentials

--- server.js
+++ server.js
@@ -3,3 +3,34 @@ const app = express()
 const bodyParser = require('body-parser')
 const mysql = require('mysql')
 const path = require('path')
+
+
+app.use(express.static('public'))
+app.use(bodyParser.json())
+
+const con = mysql.createConnection({
+	host: 'twv.energyfussl.at',
+	port: '3306',
+  user: 'Wolfg_nmgmt',
+  password: 'sX3$ma12',
+  database: 'Wolfgang_nmgmt',
+	dateStrings: true
+})
+
+con.connect(function(err){
+	if (err) throw err
+})
+
+app.listen(8008, function () {
+  console.log('Webserver listening on port 8008!')
+})
+
+app.get('/api/class/:class',function(req, res){
+  let klasse = req.params.class
+  res.send('Klasse: '+klasse)
+})
+
+app.get('/api/test/:tid',function(req, res){
+  let testid = req.params.tid
+  res.send('Test ID: '+testid)
+})

4bfd63bf901c3d297548fa4cbb107ba231c5a5ad
LeonStempfer <leon.stempfer@outlook.com>
2018-03-02 12:23:28 +0100

    Grunddesing der KlassenInfo fertig

--- public/meine_JS/serverhandler.js
+++ public/meine_JS/serverhandler.js
@@ -1,31 +1,39 @@
 let KlassenInfo={'5AHELS': [
-    {"Typ":"Test", "Fach":"Fsst","Datum":"20.2.2018","Notendurchschnitt":"3,2"},
-    {"Typ":"SA", "Fach":"Mathe","Datum":"15.2.2018","Notendurchschnitt":"2,2"},
-    {"Typ":"SMÜP", "Fach":"Englisch","Datum":"20.2.2018","Notendurchschnitt":"2,8"},
-    {"Typ":"Test", "Fach":"KSN","Datum":"24.2.2018","Notendurchschnitt":"3,6"}
+    {"Tid":"0","Typ":"Test", "Fach":"Fsst","Datum":"20.2.2018","Notendurchschnitt":"3,2"},
+    {"Tid":"1","Typ":"SA", "Fach":"Mathe","Datum":"15.2.2018","Notendurchschnitt":"2,2"},
+    {"Tid":"2","Typ":"SMÜP", "Fach":"Englisch","Datum":"20.2.2018","Notendurchschnitt":"2,8"},
+    {"Tid":"3","Typ":"Test", "Fach":"KSN","Datum":"24.2.2018","Notendurchschnitt":"3,6"}
 ],
 '4AHELS': [
-    {"Typ":"SA", "Fach":"Mathe","Datum":"15.2.2018","Notendurchschnitt":"2,2"},
-    {"Typ":"SMÜP", "Fach":"Englisch","Datum":"20.2.2018","Notendurchschnitt":"2,8"},
-    {"Typ":"Test", "Fach":"KSN","Datum":"24.2.2018","Notendurchschnitt":"3,6"}
+    {"Tid":"4","Typ":"SA", "Fach":"Mathe","Datum":"15.2.2018","Notendurchschnitt":"2,2"},
+    {"Tid":"5","Typ":"SMÜP", "Fach":"Englisch","Datum":"20.2.2018","Notendurchschnitt":"2,8"},
+    {"Tid":"6","Typ":"Test", "Fach":"KSN","Datum":"24.2.2018","Notendurchschnitt":"3,6"}
 ],
 '3AHELS': [
-    {"Typ":"Test", "Fach":"Fsst","Datum":"20.2.2018","Notendurchschnitt":"3,2"},
-    {"Typ":"SA", "Fach":"Mathe","Datum":"15.2.2018","Notendurchschnitt":"2,2"}
+    {"Tid":"7","Typ":"Test", "Fach":"Fsst","Datum":"20.2.2018","Notendurchschnitt":"3,2"},
+    {"Tid":"8","Typ":"SA", "Fach":"Mathe","Datum":"15.2.2018","Notendurchschnitt":"2,2"}
 ],
 '2AHELS': [
-    {"Typ":"SMÜP", "Fach":"Englisch","Datum":"20.2.2018","Notendurchschnitt":"2,8"},
-    {"Typ":"Test", "Fach":"KSN","Datum":"24.2.2018","Notendurchschnitt":"3,6"}
+    {"Tid":"9","Typ":"SMÜP", "Fach":"Englisch","Datum":"20.2.2018","Notendurchschnitt":"2,8"},
+    {"Tid":"10","Typ":"Test", "Fach":"KSN","Datum":"24.2.2018","Notendurchschnitt":"3,6"}
 ],
 '1AHELS': [
-    {"Typ":"Test", "Fach":"Fsst","Datum":"20.2.2018","Notendurchschnitt":"3,2"},
-    {"Typ":"SA", "Fach":"Mathe","Datum":"15.2.2018","Notendurchschnitt":"2,2"},
-    {"Typ":"Test", "Fach":"KSN","Datum":"24.2.2018","Notendurchschnitt":"3,6"}
+    {"Tid":"11","Typ":"Test", "Fach":"Fsst","Datum":"20.2.2018","Notendurchschnitt":"3,2"},
+    {"Tid":"12","Typ":"SA", "Fach":"Mathe","Datum":"15.2.2018","Notendurchschnitt":"2,2"},
+    {"Tid":"13","Typ":"Test", "Fach":"KSN","Datum":"24.2.2018","Notendurchschnitt":"3,6"}
 ] }
+
+let TestInfo={
+  "0":[{"Name":"Hans Gruber","Punktestand":"10/20","Note":"4","Bemerkung":"4-5"},
+        {"Name":"Spp","Punktestand":"13/20","Note":"3","Bemerkung":""}],
+  "1":[{"Name":"Peter","Punktestand":"11/10","Note":"1","Bemerkung":"+"}],
+  "2":[{"Name":"Fritz","Punktestand":"15/23","Note":"3","Bemerkung":"3-4"}],
+  "3":[{"Name":"Pepe","Punktestand":"13/20","Note":"3","Bemerkung":""}]
+}
 
 
 function getKlassenTest(Klasse){
-  let httpReq = new XMLHttpRequest()
+  /*let httpReq = new XMLHttpRequest()
   httpReq.open("GET", '/api/'+Klasse)
   httpReq.onload = function() {
     if(this.status != 200){
@@ -35,11 +43,11 @@ function getKlassenTest(Klasse){
       let resData = JSON.parse(this.responseText)
       callback(JSON.parse(this.responseText))
     }
-  }
-  return KlassenInfo
+  }*/
+  return KlassenInfo[Klasse]
 }
-function getSchüler(Test){
-  let schülerliste
+function getTestInfo(Test){
+  let schülerliste=TestInfo[Test]
   return schülerliste
 }
 
